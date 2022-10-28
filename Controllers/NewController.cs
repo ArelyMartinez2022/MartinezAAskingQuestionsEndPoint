6663e0cc9a0c2a9d323c21b01717e2164030ac30
arelymartinez2022 <armartinez@tracylc.net>
2022-10-27 18:56:39 -0700

    PUSH

--- Controllers/NewController.cs
+++ Controllers/NewController.cs
@@ -1,3 +1,15 @@
+/*
+ARELY MARTINEZ 
+
+OCT 25, 2022 11:23AM 
+
+ASKING QUESTION ENDPOINT
+
+THIS PROGRAM WILL MAKE IT SO THAT THE USER CAN INPUT THE TIME THEY WOKE UP AND THIER NAME. THEY WOULD INPUT THROUGH POSTMAN LIKE OUTPUT: https://localhost:7119/input/asking/arely/3PM OUTPUT: Whats up arely, you woke up at 3pm 
+
+PEER REVIEW: Pedro Castaneda -- Arely did a great job setting up her Endpoints. The route() is properly named. I think you should try adding validation to see if you can get the user to input the correct data type. Other than that your code is clean and you did a great job!
+
+*/
 using Microsoft.AspNetCore.Mvc;
 
 namespace day2ApiInput.Controllers;
