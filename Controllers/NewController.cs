2ef8726bd84d7888c3c8d0ad62235e5936a645c3
arelymartinez2022 <armartinez@tracylc.net>
2022-10-27 18:39:16 -0700

    push

---
+++ Controllers/NewController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace day2ApiInput.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NewController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("Asking")]
+    //localHost9000/Input/Name
+    public string AskingQuestions()
+    {
+        return "WHAT IS YOUR NAME? \nWHAT TIME DID YOU WAKE UP?";
+    }
+
+    [HttpGet]
+    [Route("Asking/{name}/{time}")]
+    //localHost9000/Input/Name
+    public string AskingQuestions(string name, int time)
+    {
+        return$"Whats up {name}, you woke up at {time} !";
+    }
+
+    //return$"This would be my return {var} "
+}
